09e24aaca16c61b5eaf747fafe255dce87f1af7f
Mahsa.azizi <mahsa.azizi@eligasht.com>
2018-06-18 14:33:16 +0430

    unit test sample

--- app/src/main/java/onesignal/test/com/unittestproject/MyExample.java
+++ app/src/main/java/onesignal/test/com/unittestproject/MyExample.java
@@ -1,8 +1,6 @@
 package onesignal.test.com.unittestproject;
 
-import javax.inject.Singleton;
 
-import dagger.Component;
 
 
 public interface MyExample {
--- app/src/test/java/onesignal/test/com/unittestproject/ExampleUnitTest.java
+++ app/src/test/java/onesignal/test/com/unittestproject/ExampleUnitTest.java
@@ -1,8 +1,15 @@
 package onesignal.test.com.unittestproject;
 
+import android.util.Log;
+
 import org.junit.Test;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +20,56 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void validate_test() {
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        String test = "hgd6tyghdv@ddbn.com";
+
+        assertTrue(test.matches(emailPattern));
+
+    }
+
+    @Test
+    public void array() {
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        ArrayList<String> test = new ArrayList<>();
+        test.add("a");
+        test.add("b");
+        test.add("c");
+        test.add("d");
+        test.add("e");
+        test.add("f");
+        test.add("g");
+        test.add("h@test.com");
+        assertTrue(test.get(0).equals("a"));
+        assertEquals("a", test.get(0));
+        assertTrue(test.get(test.size() - 1).matches(emailPattern));
+        assertEquals("4d252f4d9235a7a27f0758bf21a98c26",MD5("gshgdhg"));
+
+
+
+    }
+    public static String MD5(String str) {
+
+        MessageDigest digest;
+        try {
+            digest = MessageDigest.getInstance("MD5");
+            digest.reset();
+            digest.update(str.getBytes());
+            byte[] a = digest.digest();
+            int len = a.length;
+            StringBuilder sb = new StringBuilder(len << 1);
+            for (int i = 0; i < len; i++) {
+                sb.append(Character.forDigit((a[i] & 0xf0) >> 4, 16));
+                sb.append(Character.forDigit(a[i] & 0x0f, 16));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
